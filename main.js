f416d322987a5345b2df404dedda435bc3f3fafc
ProNodes11 <63229943+ProNodes11@users.noreply.github.com>
2023-07-10 17:07:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let filename = 'results-'+ currentTime + '.csv'
 async function fetchInfo(address, unicAddress) {
     try {
         const payload = '{"query":"query TransactionsTableQuery(\\n  $first: Int!\\n  $after: String\\n  $input: TransactionsInput!\\n) {\\n  ...TransactionsTablePaginationFragment_transactions_2DAjA4\\n}\\n\\nfragment TransactionsTableExpandedItemFragment_transaction on Transaction {\\n  entry_point_selector_name\\n  calldata_decoded\\n  entry_point_selector\\n  calldata\\n  initiator_address\\n  initiator_identifier\\n  main_calls {\\n    selector\\n    selector_name\\n    calldata_decoded\\n    selector_identifier\\n    calldata\\n    contract_address\\n    contract_identifier\\n    id\\n  }\\n}\\n\\nfragment TransactionsTablePaginationFragment_transactions_2DAjA4 on Query {\\n  transactions(first: $first, after: $after, input: $input) {\\n    edges {\\n      node {\\n        id\\n        ...TransactionsTableRowFragment_transaction\\n        __typename\\n      }\\n      cursor\\n    }\\n    pageInfo {\\n      endCursor\\n      hasNextPage\\n    }\\n  }\\n}\\n\\nfragment TransactionsTableRowFragment_transaction on Transaction {\\n  id\\n  transaction_hash\\n  block_number\\n  transaction_status\\n  transaction_type\\n  timestamp\\n  initiator_address\\n  initiator_identifier\\n  initiator {\\n    is_social_verified\\n    id\\n  }\\n  main_calls {\\n    selector_identifier\\n    id\\n  }\\n  ...TransactionsTableExpandedItemFragment_transaction\\n}\\n","variables":{"first":30,"after":null,"input":{"initiator_address":"' + address + '","sort_by":"timestamp","order_by":"desc","min_block_number":null,"max_block_number":null,"min_timestamp":null,"max_timestamp":null}}}';
+        const balance_payload = '{"query":"query ERC20BalancesByOwnerAddressTableQuery(\\n  $input: ERC20BalancesByOwnerAddressInput!\\n) {\\n  erc20BalancesByOwnerAddress(input: $input) {\\n    id\\n    ...ERC20BalancesByOwnerAddressTableRowFragment_erc20Balance\\n  }\\n}\\n\\nfragment ERC20BalancesByOwnerAddressTableRowFragment_erc20Balance on ERC20Balance {\\n  id\\n  contract_address\\n  contract_erc20_identifier\\n  contract_erc20_contract {\\n    symbol\\n    is_social_verified\\n    icon_url\\n    id\\n  }\\n  balance_display\\n}\\n","variables":{"input":{"owner_address":"' + address + '"}}}';
         const headers = {
             'Authority': 'starkscan.stellate.sh',
             'Accept': 'application/json',
@@ -27,9 +28,20 @@ async function fetchInfo(address, unicAddress) {
             method: 'POST',
             headers: headers,
             body: payload
-          })
+        })
+        const balances_response = await  fetch('https://starkscan.stellate.sh/', {
+            method: 'POST',
+            headers: headers,
+            body: balance_payload
+        })
+        let nonce
+        let balanceString = ''
+        const balances = await balances_response.json()
+        for (let balance of balances.data.erc20BalancesByOwnerAddress) {
+            balanceString += `${balance.contract_erc20_contract.symbol} ${balance.balance_display} | `;
+        }
         const data = await response.json()
-        let  nonce
+       
         let firstDay = '2053-05-19T15:51:55.000Z'
         let lastDay  = '2000-05-19T15:51:55.000Z'
         for (let tx of data.data.transactions.edges) {
@@ -49,7 +61,8 @@ async function fetchInfo(address, unicAddress) {
             }
             nonce = data.data.transactions.edges.length
         }
-        addRowToCSV(address, nonce, unicAddress.length + 1, await formatDate(firstDay), await formatDate(lastDay),  await dateDiff(firstDay, lastDay))
+        console.log(balanceString)
+        addRowToCSV(address, balanceString, nonce, unicAddress.length + 1, await formatDate(firstDay), await formatDate(lastDay),  await dateDiff(firstDay, lastDay))
     } catch (error) {
         console.log(`Address ${address} Failed`.red)
     }
@@ -69,8 +82,8 @@ async function dateDiff(dateStart, dateEnd) {
     return diffDays
 }
 
-function addRowToCSV(address, nonce, interactedAddress, firstDay, lastDay, dayDiff) {
-    const row = `${address},${nonce},${interactedAddress},${firstDay},${lastDay},${dayDiff}\n`;
+function addRowToCSV(address, balance, nonce, interactedAddress, firstDay, lastDay, dayDiff) {
+    const row = `${address},${balance},${nonce},${interactedAddress},${firstDay},${lastDay},${dayDiff}\n`;
     fs.appendFile(filename, row, (err) => {
     });
 }
@@ -78,7 +91,7 @@ function addRowToCSV(address, nonce, interactedAddress, firstDay, lastDay, dayDi
 
 
 async function main() {
-    addRowToCSV('Address', 'TxCount', "Interacted address", "First Day", "Last Day", "Days count")
+    addRowToCSV('Address', 'Balance', 'TxCount', "Interacted address", "First Day", "Last Day", "Days count")
     let counter = 1
     for (let wallet of wallets){
         process.stdout.clearLine();  
